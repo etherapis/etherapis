23854a71eeca30f384df43c9f8c972a356075779
Péter Szilágyi <peterke@gmail.com>
2016-02-12 17:33:15 +0200

    dashboard/assets: add progress bar to monitor sync progress

--- etherapis/dashboard/assets/components/ethereum.js
+++ etherapis/dashboard/assets/components/ethereum.js
@@ -10,15 +10,21 @@ var EthereumStats = React.createClass({
   // getInitialState sets the zero values of the component.
   getInitialState: function() {
     return {
-      peers: [],
-      head:  null,
+      syncing: false,
+      peers:   [],
+      head:    null,
     };
   },
   // componentDidMount is invoked when the status component finishes loading. It
   // starts the periodical refresh of its internal state based on the backend API.
   componentDidMount: function() {
+    this.refreshSyncing(); setInterval(this.refreshSyncing, this.props.refresh);
     this.refreshPeers(); setInterval(this.refreshPeers, this.props.refresh);
     this.refreshHead(); setInterval(this.refreshHead, this.props.refresh);
+  },
+  // refreshSyncing retrieves the sync status/progress of the Ethereum client.
+  refreshSyncing: function() {
+    this.props.ajax(this.props.apiurl + "/syncing", function(syncing) { this.setState({syncing: syncing}); }.bind(this));
   },
   // refreshPeers retrieves the list of connected peers and injects them into
   // the local state for sub-component interpretation.
@@ -32,15 +38,59 @@ var EthereumStats = React.createClass({
   // render flattens the Ethereum stats into UI report objects.
   render: function() {
     return (
-      <span>
-        <PeerCounter peers={this.state.peers}/>
-        <BlockNumber block={this.state.head}/>
-        <BlockTimer block={this.state.head}/>
-      </span>
+      <div className="navbar-inner" style={{height: "50px"}}>
+        <div className="navbar-right">
+          <PeerCounter peers={this.state.peers}/>
+          <BlockNumber block={this.state.head}/>
+          <BlockTimer block={this.state.head}/>
+        </div>
+        <SyncProgress syncing={this.state.syncing} head={this.state.head}/>
+      </div>
     );
   }
-})
+});
 window.EthereumStats = EthereumStats // Expose the component
+
+// SyncProgress is a UI component that displays the current sync status of the
+// Ethereum node as a progress bar in the navbar. The bar is only displayed if
+// there's actually a sync in progress.
+//
+// Properties:
+//   syncing: Ethereum syncing progress
+//   head:    Head block (to decide if initial sync or not)
+var SyncProgress = React.createClass({
+  render: function() {
+    if (this.props.syncing == false) {
+      return null;
+    }
+    var start   = parseInt(this.props.syncing.startingBlock, 16);
+    var height  = parseInt(this.props.syncing.highestBlock, 16);
+    var current = parseInt(this.props.syncing.currentBlock, 16);
+    var pulled  = parseInt(this.props.syncing.pulledStates, 16)
+    var known   = parseInt(this.props.syncing.knownStates, 16);
+
+    var progress = 100 * (current - start) / (height - start);
+
+    var label = "Sync: "
+    if (this.props.head.number == 0) {
+      label = "Initial sync: "
+    }
+    if (known - pulled > 0) {
+      label += (height - current) + " blocks, " + (known - pulled) + "/" + known + " states left";
+    } else {
+      label += (height - current) + " blocks left";
+    }
+    return (
+      <div style={{paddingTop: "15px"}}>
+        <div className="progress" style={{position: "relative", height: "20px"}}>
+          <div className="progress-bar progress-bar-striped active" role="progressbar" style={{width: progress + "%"}}>
+            <span style={{position: "absolute", display: "block", width: "100%"}}><small>{label}</small></span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 
 // PeerCounter is a UI component that displays the number of Ethereum peers we
 // are currently connected to.
